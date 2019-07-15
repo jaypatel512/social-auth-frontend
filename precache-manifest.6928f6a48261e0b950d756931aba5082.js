2dbe11d74b033734eb2056741ad929563595ee13
jaypatel512 <jaypatel512@gmail.com>
2019-07-15 00:36:54 -0500

    Updates

---
+++ precache-manifest.6928f6a48261e0b950d756931aba5082.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0900aa19c324652b83249f4b18b36e",
+    "url": "/social-auth-frontend/index.html"
+  },
+  {
+    "revision": "d955a85ac945327ba5fd",
+    "url": "/social-auth-frontend/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "321aa68b13c2aa7fdad5",
+    "url": "/social-auth-frontend/static/js/2.54eaddb3.chunk.js"
+  },
+  {
+    "revision": "d955a85ac945327ba5fd",
+    "url": "/social-auth-frontend/static/js/main.0b9aceb4.chunk.js"
+  },
+  {
+    "revision": "27622979ea1c1b3b52be",
+    "url": "/social-auth-frontend/static/js/runtime~main.8f037264.js"
+  }
+]);
