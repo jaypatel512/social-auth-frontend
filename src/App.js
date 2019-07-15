48781f9cd1a989fc6a5b2a79e4b2d24ce39f5cdf
jaypatel512 <jaypatel512@gmail.com>
2019-07-15 01:02:01 -0500

    Use social path

--- src/App.js
+++ src/App.js
@@ -1,93 +1,86 @@
-import React from 'react';
-import './App.css';
-import GoogleLogin from 'react-google-login';
-import config from './config'
+import React from "react";
+import "./App.css";
+import GoogleLogin from "react-google-login";
+import config from "./config";
 
 class App extends React.Component {
-
   state = {
     isAuthenticated: false,
     user: null,
-    token: ''
-  }
+    token: ""
+  };
 
   logout = () => {
     this.setState({
       isAuthenticated: false,
       user: null,
-      token: ''
-    })
-  }
+      token: ""
+    });
+  };
 
-  googleResponse = (response) => {
+  googleResponse = response => {
     console.log(response);
     const { accessToken } = response;
 
     const tokenBlob = new Blob(
-      [JSON.stringify({
-        access_token: accessToken
-      }, null, 2)],
-      { type: 'application/json' }
-    )
+      [
+        JSON.stringify(
+          {
+            access_token: accessToken
+          },
+          null,
+          2
+        )
+      ],
+      { type: "application/json" }
+    );
 
-    console.log(tokenBlob)
+    console.log(tokenBlob);
 
     const options = {
-      method: 'POST',
+      method: "POST",
       body: tokenBlob,
-      mode: 'cors',
-      cache: 'default'
-    }
+      mode: "cors",
+      cache: "default"
+    };
 
-    fetch('http://localhost:4000/api/v1/auth/google', options)
-      .then(r => {
-        const token = r.headers.get('x-auth-token');
-        r.json()
-          .then(user => {
-            if (token) {
-              this.setState({
-                isAuthenticated: true,
-                user,
-                token
-              })
-            }
-          })
-      })
-  }
+    fetch("https://social-auth-tutorial.herokuapp.com/api/v1/auth/google", options).then(r => {
+      const token = r.headers.get("x-auth-token");
+      r.json().then(user => {
+        if (token) {
+          this.setState({
+            isAuthenticated: true,
+            user,
+            token
+          });
+        }
+      });
+    });
+  };
 
-  onFailure = (err) => {
+  onFailure = err => {
     alert(err);
-  }
+  };
 
   render() {
-    let content = !!this.state.isAuthenticated ?
-      (
+    let content = !!this.state.isAuthenticated ? (
+      <div>
+        <div>Authenticated</div>
         <div>
-          <div>Authenticated</div>
-          <div>
-            <p>{this.state.user.email}</p>
-            <button onClick={this.logout}>Logout</button>
-          </div>
+          <p>{this.state.user.email}</p>
+          <button onClick={this.logout}>Logout</button>
         </div>
-      ) :
-      (
-        <div>
-          <GoogleLogin
-            clientId={config.GOOGLE_CLIENT_ID}
-            buttonText="Login With Google"
-            onSuccess={this.googleResponse}
-            onFailure={this.googleResponse}
-          />
-        </div>
-      );
+      </div>
+    ) : (
+      <div>
+        <GoogleLogin clientId={config.GOOGLE_CLIENT_ID} buttonText="Login With Google" onSuccess={this.googleResponse} onFailure={this.googleResponse} />
+      </div>
+    );
 
-    return <div className='App'>
-      {content}
-    </div>
+    return <div className="App">{content}</div>;
   }
 }
 
 export default App;
-
 
 // Secret: 4h67stmhkV3FuH4c_7pRvshl
